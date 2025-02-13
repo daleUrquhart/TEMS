ed019d14d759a999a8193bf6a40ad315dd14e19e
daleUrquhart <daleurquhart1@gmail.com>
2025-02-13 01:26:28 -0400

    Built Maven project, and drafted starter documentation

---
+++ main/src/main/java/com/tems/App.java
@@ -0,0 +1,13 @@
+package com.tems;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
