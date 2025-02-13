46b4e98f5952b1e6d07a616359ab639a61a0020d
daleUrquhart <daleurquhart1@gmail.com>
2025-02-13 04:11:42 -0400

    Updated Displayable.java

---
+++ main/src/main/java/com/tems/Displayable.java
@@ -0,0 +1,16 @@
+package com.tems;
+
+import javafx.scene.layout.HBox;
+
+/**
+ * Interface for getDisplay 
+ */
+public interface Displayable {
+    
+    /**
+     * Builds and returns a HBox display of teh displayable
+     * @return displayType (0 for TR view, 1 for displayable view)
+     * @return HBox representing the displayable with available actions
+     */
+    public HBox getDisplay(int displayType);
+}
