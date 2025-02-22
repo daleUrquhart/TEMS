927f2fd09de68792c0951562de35a353c9f3cb3d
daleUrquhart <daleurquhart1@gmail.com>
2025-02-22 14:10:36 -0400

    Updated schema for gender management

---
+++ main/src/databse/load_genders.sql
@@ -0,0 +1,10 @@
+INSERT INTO Genders (gender_name) VALUES 
+('Male'), 
+('Female'), 
+('Non-Binary'), 
+('Other'), 
+('Transgender'), 
+('Gender Fluid'), 
+('Agender'), 
+('Bigender'), 
+('Two-Spirit');
