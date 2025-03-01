2c5f0d85f91c0bbb9e035fc4719d109920609db3
daleUrquhart <daleurquhart1@gmail.com>
2025-03-01 17:11:36 -0400

    Updated sample data

--- src/main/java/com/tems/util/SampleData.java
+++ src/main/java/com/tems/util/SampleData.java
@@ -2,6 +2,8 @@ package com.tems.util;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.tems.models.Auditionee;
 import com.tems.models.CriteriaType;
@@ -13,21 +15,39 @@ import com.tems.models.TalentRecruiter;
 public class SampleData {
     public static void load() {
         try {
-            Auditionee.create("John", "John@applicant.ca", "Password", Gender.MALE, 1);
-            int trId = TalentRecruiter.create("John", "john@company.ca", "Password", "Company 1");
+            String hash = PasswordManager.hashPassword("a");
+
+            // Define gender roles
+            ArrayList<Gender> genderRoles = new ArrayList<>();
+            genderRoles.add(Gender.MALE);
+            genderRoles.add(Gender.FEMALE);
+
+            // Define Criteria and Weights
+            Map<CriteriaType, Integer> selectedCriteria = new HashMap<>();
+            selectedCriteria.put(CriteriaType.PHYSICAL_APPEARANCE, 1);
+
+            // Create auditionee
+            Auditionee.create("John", "John@applicant.ca", hash, Gender.MALE, 1, genderRoles);
+
+            // Create Talent Recruiter
+            int trId = TalentRecruiter.create("John", "r@r.ca", hash, "Company 1");
+
+            // Define Genders
             ArrayList<Gender> genders = new ArrayList<>();
             genders.add(Gender.MALE);
-            ArrayList<Genre> genres = new ArrayList<>();
-            genres.add(Genre.ACTION);  
-            ArrayList<CriteriaType> criteriaTypes = new ArrayList<>();
-            int[] weights = new int[]{1};
-            criteriaTypes.add(CriteriaType.PHYSICAL_APPEARANCE);
             ArrayList<Gender> genders2 = new ArrayList<>();
             genders2.add(Gender.FEMALE);
-            // Valid Listing
-            Listing.create(trId, "James Bond", "James Bond role for the new James Bond movie by Movie Co.", genders, genres, criteriaTypes, weights);
-            Listing.create(trId, "Alice Bond", "Alice Bond role for the new James Bond movie by Movie Co.", genders2, genres, criteriaTypes, weights);
+
+            // Define Genres
+            ArrayList<Genre> genres = new ArrayList<>();
+            genres.add(Genre.ACTION);
+
+            // Create Listings
+            Listing.create(trId, "James Bond", "James Bond role for the new James Bond movie by Movie Co.", genders, genres, selectedCriteria);
+            Listing.create(trId, "Alice Bond", "Alice Bond role for the new James Bond movie by Movie Co.", genders2, genres, selectedCriteria);
+
         } catch (SQLException e) {
-        }    
+            System.err.println("Error in SampleData.java: \n\t"+e.getMessage());
+        }
     }
 }
